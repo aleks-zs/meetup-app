4044b361b689b62f9f965dcd6cdffcf1daff81e1
Aleks <aleks@JustComputer.local>
2023-01-19 15:35:52 +0200

    testing

--- app/meetup/urls.py
+++ app/meetup/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<slug:slug>/success', views.confirm_registration, name='confirm-registration'), # noqa
-    path('<slug:slug>', views.meetup_details, name='meetup-detail')
+    path('<slug:slug>', views.meetup_details, name='meetup-detail'),
+    path('<slug:slug>/success', views.confirm_registration, name='confirm-registration'),  # noqa
 ]
